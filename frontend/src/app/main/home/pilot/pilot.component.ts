6a4119c7c10aa8e8e43fe9768a6e7f3cdfdb0559
DavidSeo4 <seodavid4@gmail.com>
2023-09-29 14:25:46 +0200

    New league keys OK

---
+++ frontend/src/app/main/home/pilot/pilot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pilot',
+  templateUrl: './pilot.component.html',
+  styleUrls: ['./pilot.component.scss']
+})
+export class PilotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

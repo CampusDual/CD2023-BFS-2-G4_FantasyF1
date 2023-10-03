77c212d3cc4865a7a7f439644c64194819b11cdd
Leticia Prieto <lettieprieto@gmail.com>
2023-10-03 10:31:35 +0200

    number of users registered added to home table

--- backend/backend-model/src/main/java/com/grupo4/model/core/service/CompetitionService.java
+++ backend/backend-model/src/main/java/com/grupo4/model/core/service/CompetitionService.java
@@ -32,11 +32,7 @@ public class CompetitionService implements ICompetitionService {
     }
 
     public EntityResult publicCountCompetitionQuery(Map<String, Object> keysValues, List<String> attrMap) throws OntimizeJEERuntimeException {
-        EntityResult r =  this.daoHelper.query(this.competitionDao, keysValues, attrMap, "publicCount");
-
-        r.getRecordIndex();
-
-        return null;
+        return r this.daoHelper.query(this.competitionDao, keysValues, attrMap, "publicCount");
     }
 
     public EntityResult allCountCompetitionQuery(Map<String, Object> keysValues, List<String> attrMap) throws OntimizeJEERuntimeException {

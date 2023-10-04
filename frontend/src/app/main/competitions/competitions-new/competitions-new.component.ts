62a4962f52da96c2fd38484f8094291b90d418c2
DavidSeo4 <seodavid4@gmail.com>
2023-10-04 11:53:06 +0200

    League code

--- frontend/src/app/main/competitions/competitions-new/competitions-new.component.ts
+++ frontend/src/app/main/competitions/competitions-new/competitions-new.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { OComboComponent, OTextInputComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-new_competition',
@@ -7,18 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompetitionsNewComponent implements OnInit {
 
+  @ViewChild('type_combo', {static:true}) type_combo: OComboComponent;
+  @ViewChild('league_code', {static:false}) league_code: OTextInputComponent;
+
+  public isPrivate: boolean = false;
+
   public staticData = [{
     value: true,
-    name: 'PUBLIC'
+    name: "PUBLIC"
   }, {
     value: false,
-    name: 'PRIVATE'
+    name: "PRIVATE"
   }];
 
+  public texto: string = this.asignarVar();
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+  }
+
+  asignarVar(): string{
+    const date = new Date();
+    let varia:string = `${date}-${"nombreUsuario"}`;
+    return varia;
+  }
+
+  changeType() {
+    if(this.type_combo.getValue()===false){
+      this.isPrivate=true;
+      this.league_code.setValue(this.texto);
+    } else{
+      this.isPrivate=false;
+      this.league_code.setValue("");
+    }
   }
 
 

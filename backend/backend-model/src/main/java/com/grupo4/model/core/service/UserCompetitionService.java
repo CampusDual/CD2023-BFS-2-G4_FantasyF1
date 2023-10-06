d814cdb814ce7191ccc477983ee346ba139e9ada
Leticia Prieto <lettieprieto@gmail.com>
2023-10-06 11:51:30 +0200

    error in user_comp query solved

--- backend/backend-model/src/main/java/com/grupo4/model/core/service/UserCompetitionService.java
+++ backend/backend-model/src/main/java/com/grupo4/model/core/service/UserCompetitionService.java
@@ -36,7 +36,9 @@ public class UserCompetitionService implements IUserCompetitionService {
 
     @Override
     public EntityResult userCompetitionFilterQuery(Map<String, Object> keysValues, List<String> attrMap) throws OntimizeJEERuntimeException {
-        return this.daoHelper.query(this.userCompetitionDao, keysValues, attrMap);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        keysValues.put(UserDao.ID, authentication.getName());
+        return this.daoHelper.query(this.userCompetitionDao, keysValues, attrMap,"filter");
     }
 
     @Override

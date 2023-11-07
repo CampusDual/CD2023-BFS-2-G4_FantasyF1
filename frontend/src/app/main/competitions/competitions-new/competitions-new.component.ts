47ea9ba2c7252e51acdf9f99e9b44cf6d196f2fc
Leticia Prieto <lettieprieto@gmail.com>
2023-11-07 14:44:01 +0100

    unifying styles, trying to add competition images

--- frontend/src/app/main/competitions/competitions-new/competitions-new.component.ts
+++ frontend/src/app/main/competitions/competitions-new/competitions-new.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
-import { OComboComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OComboComponent, OTextInputComponent, OTranslateService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-new_competition',
@@ -8,42 +8,59 @@ import { OComboComponent, OTextInputComponent } from 'ontimize-web-ngx';
 })
 export class CompetitionsNewComponent implements OnInit {
 
-  @ViewChild('type_combo', {static:true}) type_combo: OComboComponent;
-  @ViewChild('league_code', {static:false}) league_code: OTextInputComponent;
+  @ViewChild('type_combo', { static: true }) type_combo: OComboComponent;
+  @ViewChild('league_code', { static: false }) league_code: OTextInputComponent;
 
   public isPrivate: boolean = false;
 
   public staticData = [{
     value: true,
-    name: "PUBLIC"
+    name:this.translator.get("PUBLIC")
   }, {
     value: false,
-    name: "PRIVATE"
+    name: this.translator.get("PRIVATE")
+  }];
+
+  public staticColors = [{
+    value: "assets/images/comp_img_red.png",
+    name: this.translator.get("RED")
+  },
+  {
+    value: "assets/images/comp_img_blue.png",
+    name: this.translator.get("BLUE")
+  }, 
+  {
+    value: "assets/images/comp_img_green.png",
+      name: this.translator.get("GREEN")
+  }, 
+  {
+      value: "assets/images/comp_img_black.png",
+      name: this.translator.get("BLACK")
   }];
 
   public texto: string = this.asignarVar();
 
-  constructor() {
-   }
+  constructor(private translator: OTranslateService) {
+  }
 
   ngOnInit() {
   }
 
-  asignarVar(): string{
+  asignarVar(): string {
     const date = new Date();
     let sessionData = localStorage.getItem("com.ontimize.web.ngx.jee.seed");
     let sessionUser = JSON.parse(sessionData).session["user"];
     let numRandom = Math.floor(Math.random() * (10000 - 0 + 1)) + 0;
-    let fecha:string = `${date.getHours()}${date.getMinutes()}${date.getSeconds()}${numRandom}${date.getDate()}${date.getMonth()}${date.getFullYear()}` 
-    let code:string = `${fecha}${sessionUser}`;
+    let fecha: string = `${date.getHours()}${date.getMinutes()}${date.getSeconds()}${numRandom}${date.getDate()}${date.getMonth()}${date.getFullYear()}`
+    let code: string = `${fecha}${sessionUser}`;
     return code;
   }
 
   changeType() {
-    if(this.type_combo.getValue()===false){
+    if (this.type_combo.getValue() === false) {
       document.getElementById('codigo').classList.remove("code_form");
       this.league_code.setValue(this.texto);
-    } else{
+    } else {
       document.getElementById('codigo').classList.add("code_form")
       this.league_code.setValue("");
 
--- frontend/src/app/shared/app.menu.config.ts
+++ frontend/src/app/shared/app.menu.config.ts
@@ -3,6 +3,5 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'COMP_USER_TITLE', icon: 'home', route: '/main/home' },
   { id: 'available', name: 'COMP_AVAILABLE_MENU', icon: 'people', route: '/main/competitions/available' },
-  { id: 'search', name: 'PRIVATE_LEAGUE', icon: 'search', route: '/main/competitions/search' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
